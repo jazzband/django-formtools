171e3c63a0246fb5b955dfc0fc017e480969256f
Tim Graham <timograham@gmail.com>
2017-02-03 09:32:14 -0500

    Add testing for Python 3.6

--- setup.py
+++ setup.py
@@ -140,6 +140,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
     ],
     zip_safe=False,
