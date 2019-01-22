9c8ea543c81b26c6c8c63ce1fda6480ab0b79e83
Tim Graham <timograham@gmail.com>
2019-01-22 15:13:39 -0500

    Add testing for Python 3.7

--- setup.py
+++ setup.py
@@ -140,6 +140,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
     ],
     zip_safe=False,
