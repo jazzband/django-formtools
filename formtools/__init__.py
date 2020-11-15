908359d0978e185e493a5f65f616350b931789a1
David Smith <smithdc@gmail.com>
2020-11-15 18:26:26 +0000

    Removed default default_app_config setting for Django>=3.2 deault_app_config will be deprecated in Django 3.2.

--- formtools/__init__.py
+++ formtools/__init__.py
@@ -1,3 +1,6 @@
+import django
+
 __version__ = '2.2'
 
-default_app_config = 'formtools.apps.FormToolsConfig'
+if django.VERSION <= (3, 2):
+    default_app_config = 'formtools.apps.FormToolsConfig'
