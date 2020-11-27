0fa9ce5c6edced350807d0383971e2930a298582
Jannis Leidel <jannis@leidel.info>
2020-11-27 21:48:14 +0100

    Use setuptools_scm.

--- formtools/__init__.py
+++ formtools/__init__.py
@@ -1,6 +1,11 @@
 import django
+from pkg_resources import get_distribution, DistributionNotFound
 
-__version__ = '2.2'
+try:
+    __version__ = get_distribution("django-formtools").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = None
 
 if django.VERSION <= (3, 2):
     default_app_config = 'formtools.apps.FormToolsConfig'
