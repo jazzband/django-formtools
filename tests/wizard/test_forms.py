jazzband/django-formtools

commit 533a83053deab2adeb72de079e813aae78b03c1a

Fixes #168 - Use get_form_list() instead of form_list
Co-authored-by: Matthew Schinckel <matt@schinckel.net>

tests/wizard/test_forms.py
@@ -91,6 +91,14 @@ class TestWizardWithTypeCheck(TestWizard):
         return http.HttpResponse("All good")
 
 
+class TestWizardWithCustomGetFormList(TestWizard):
+
+    form_list = [Step1]
+
+    def get_form_list(self):
+        return {'start': Step1, 'step2': Step2}
+
+
 class FormTests(TestCase):
     def test_form_init(self):
         testform = TestWizard.get_initkwargs([Step1, Step2])
@@ -256,6 +264,25 @@ class FormTests(TestCase):
         testform = TestWizardWithTypeCheck.as_view([('start', Step1)])
         response, instance = testform(request)
         self.assertEqual(response.status_code, 200)
+
+    def test_get_form_list_default(self):
+        request = get_request()
+        testform = TestWizard.as_view([('start', Step1)])
+        response, instance = testform(request)
+
+        form_list = instance.get_form_list()
+        self.assertEqual(form_list, {'start': Step1})
+        with self.assertRaises(KeyError):
+            instance.get_form('step2')
+
+    def test_get_form_list_custom(self):
+        request = get_request()
+        testform = TestWizardWithCustomGetFormList.as_view([('start', Step1)])
+        response, instance = testform(request)
+
+        form_list = instance.get_form_list()
+        self.assertEqual(form_list, {'start': Step1, 'step2': Step2})
+        self.assertIsInstance(instance.get_form('step2'), Step2)
 
 
 class SessionFormTests(TestCase):

formtools/wizard/views.py
@@ -406,7 +406,7 @@ class WizardView(TemplateView):
         """
         if step is None:
             step = self.steps.current
-        form_class = self.form_list[step]
+        form_class = self.get_form_list()[step]
         # prepare the kwargs for the form instance.
         kwargs = self.get_form_kwargs(step)
         kwargs.update({

commit c6d9eda3c1134c2001cac62472826967858b06c7

Add test_form_condition_callable
Skipped for now as this feature broke in 533a83053deab2adeb72de079e813aae78b03c1a

see: https://github.com/jazzband/django-formtools/issues/220

tests/wizard/test_forms.py
@@ -1,3 +1,4 @@
+import unittest
 from importlib import import_module
 
 from django import forms, http
@@ -157,6 +158,34 @@ class FormTests(TestCase):
         )
         response, instance = testform(request)
         self.assertEqual(instance.get_next_step(), 'step3')
+
+    @unittest.skip('https://github.com/jazzband/django-formtools/issues/220')
+    def test_form_condition_callable(self):
+        def step2_is_enabled(wizard):
+            cleaned_data = wizard.get_cleaned_data_for_step('start')
+            if cleaned_data:
+                return cleaned_data.get('name') == 'yes'
+
+        testform = TestWizard.as_view(
+            [('start', Step1), ('step2', Step2), ('step3', Step3)],
+            condition_dict={'step2': step2_is_enabled}
+        )
+        request = get_request({'test_wizard-current_step': 'step1', 'start-name': 'yes'})
+        response, instance = testform(request)
+        # being on step2 when giving name "yes"
+        self.assertEqual(instance.get_prev_step(), 'start')
+        self.assertEqual(instance.steps.current, 'step2')
+        self.assertEqual(instance.get_next_step(), 'step3')
+        self.assertEqual(instance.get_step_index(), 1)
+
+        request = get_request({'test_wizard-current_step': 'start', 'start-name': 'no'})
+        response, instance = testform(request)
+        # step2 being skipped and no more steps to go when giving name "no"
+        self.assertEqual(instance.get_prev_step(), 'start')
+        self.assertEqual(instance.steps.current, 'step3')
+        self.assertEqual(instance.get_next_step(), None)
+        # index is still 1, because step2 is not in list returned by get_form_list()
+        self.assertEqual(instance.get_step_index(), 1)
 
     def test_form_condition_unstable(self):
         request = get_request()